4314613974d7e5a2cb255e7fe6edc098ee73572d
ihsan <hasansaygin24@gmail.com>
2023-03-22 13:22:22 +0300

    serializers done

---
+++ car/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from .models import Car, Reservation
+
+
+class CarSerializer(serializers.ModelSerializer):
+    is_available = serializers.BooleanField()
+    class Meta:
+        model = Car
+        fields = (
+            'id',
+            'plate_number',
+            'brand',
+            'model',
+            'year',
+            'gear',
+            'rent_per_day',
+            'availability',
+            'is_available'
+        )
+    # Bu method u override etmek yerine staff ve normal userlar için ayrı ayrı serializerlar oluşturulup
+    # View de get_serializer_class methode u override edilerek user a göre serializerlar seçilebilir. Plaka numarası ve müsaitlik durumunu sadece staff olan kullanıcı görebilir
+
+    def get_fields(self):
+        fields = super().get_fields()
+        request = self.context.get('request')
+
+        if request.user and not request.user.is_staff:
+            fields.pop('availability')
+            fields.pop('plate_number')
+        return fields
+
+class ReservationSerializer(serializers.ModelSerializer):
+    total_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Reservation
+        fields = (
+            'id',
+            'customer',
+            'car',
+            'start_date',
+            'end_date',
+            'total_price'
+        )
+
+    def get_total_price(self, obj):
+        return obj.car.rent_per_day * (obj.end_date - obj.start_date).days
